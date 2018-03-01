2fbef705ba5245d9bac2e11ac009b815b746054d
luecy1 <kowaheno@gmail.com>
2018-03-02 06:07:15 +0900

    今朝の進捗をコミット

--- app/src/main/java/com/github/luecy1/basicsample/ui/ProductListFragment.java
+++ app/src/main/java/com/github/luecy1/basicsample/ui/ProductListFragment.java
@@ -1,5 +1,6 @@
 package com.github.luecy1.basicsample.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,18 +8,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.luecy1.basicsample.R;
+import com.github.luecy1.basicsample.databinding.ListFragmentBinding;
+
 /**
  * Created by you on 2018/02/24.
  */
 // TODO
 public class ProductListFragment extends Fragment {
 
-    public static final String TAG = "productListViewModel";
+    public static final String TAG = "ProductListViewModel";
+
+    private ListFragmentBinding mBinding;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.list_fragment, container, false);
+
+        return mBinding.getRoot();
     }
 
     @Override
