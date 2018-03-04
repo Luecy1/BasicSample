c4792c6043c683c28f5c0c461e2711d77293d5ff
luecy1 <kowaheno@gmail.com>
2018-03-04 17:15:32 +0900

    今日の進捗をコミット

--- app/src/main/java/com/github/luecy1/basicsample/ui/ProductAdapter.java
+++ app/src/main/java/com/github/luecy1/basicsample/ui/ProductAdapter.java
@@ -2,6 +2,7 @@ package com.github.luecy1.basicsample.ui;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import com.github.luecy1.basicsample.model.Product;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by you on 2018/03/03.
@@ -23,6 +25,44 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
     public ProductAdapter(@Nullable ProductClickCallback clickCallback) {
         this.mProductClickCallback = clickCallback;
+    }
+
+    public void setProductList(final List<? extends Product> productList) {
+        if (mProductList == null) {
+            this.mProductList = productList;
+            notifyItemRangeInserted(0, productList.size());
+        } else {
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return mProductList.size();
+                }
+
+                @Override
+                public int getNewListSize() {
+                    return productList.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return mProductList.get(oldItemPosition).getId() ==
+                            productList.get(newItemPosition).getId();
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    Product newProduct = productList.get(newItemPosition);
+                    Product oldProduct = productList.get(oldItemPosition);
+                    return newProduct.getId() == oldProduct.getId()
+                            && Objects.equals(newProduct.getDescription(), oldProduct.getDescription())
+                            && Objects.equals(newProduct.getNage(), oldProduct.getNage())
+                            && newProduct.getPrice() == oldProduct.getPrice();
+                }
+            });
+            mProductList = productList;
+            result.dispatchUpdatesTo(this);
+        }
+
     }
 
     @NonNull
