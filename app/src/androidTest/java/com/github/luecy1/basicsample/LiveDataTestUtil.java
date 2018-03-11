308c5bdd845cde4eba2e8496f185cdc4e52c22ce
luecy1 <kowaheno@gmail.com>
2018-03-12 06:02:37 +0900

    今朝の進捗をコミット

---
+++ app/src/androidTest/java/com/github/luecy1/basicsample/LiveDataTestUtil.java
@@ -0,0 +1,32 @@
+package com.github.luecy1.basicsample;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+
+import java.util.Objects;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by you on 2018/03/12.
+ */
+public class LiveDataTestUtil {
+
+    public static <T> T getValue(final LiveData<T> liveData) throws InterruptedException {
+        final Objects[] data = new Objects[1];
+        final CountDownLatch latch = new CountDownLatch(1);
+        Observer<T> observer = new Observer<T>() {
+            @Override
+            public void onChanged(@Nullable T o) {
+                data[0] = (Objects) o;
+                latch.countDown();
+                liveData.removeObserver(this);
+            }
+        };
+        liveData.observeForever(observer);
+        latch.await(2, TimeUnit.SECONDS);
+
+        return (T) data[0];
+    }
+}
