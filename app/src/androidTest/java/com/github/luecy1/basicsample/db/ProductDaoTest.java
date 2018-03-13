f5d78ed906cf45262aa7e197da4067f35461876e
luecy1 <kowaheno@gmail.com>
2018-03-13 22:11:33 +0900

    今日の進捗をコミット

--- app/src/androidTest/java/com/github/luecy1/basicsample/db/ProductDaoTest.java
+++ app/src/androidTest/java/com/github/luecy1/basicsample/db/ProductDaoTest.java
@@ -17,12 +17,15 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
+import static com.github.luecy1.basicsample.db.TestData.PRODUCTS;
+import static com.github.luecy1.basicsample.db.TestData.PRODUCT_ENTRY;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by you on 2018/03/13.
  */
-// TODO
 @RunWith(AndroidJUnit4.class)
 public class ProductDaoTest {
 
@@ -56,4 +59,24 @@ public class ProductDaoTest {
 
     }
 
+    @Test
+    public void getProductsAfterInserted() throws InterruptedException {
+        mProductDao.insertAll(PRODUCTS);
+
+        List<ProductEntry> products = LiveDataTestUtil.getValue(mProductDao.loadAllProducts());
+
+        assertThat(products.size(), is(PRODUCTS.size()));
+    }
+
+    @Test
+    public void getProductById() throws InterruptedException {
+        mProductDao.insertAll(PRODUCTS);
+
+        ProductEntry product = LiveDataTestUtil.getValue(mProductDao.loadProduct(PRODUCT_ENTRY.getId()));
+
+        assertThat(product.getId(), is(PRODUCT_ENTRY.getId()));
+        assertThat(product.getName(), is(PRODUCT_ENTRY.getName()));
+        assertThat(product.getDescription(), is(PRODUCT_ENTRY.getDescription()));
+        assertThat(product.getPrice(), is(PRODUCT_ENTRY.getPrice()));
+    }
 }
